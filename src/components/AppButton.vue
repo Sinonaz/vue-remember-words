853ab632b66aa174d6c46cb4bcf45e4f6fbd7301
Isaac <aziksimpson@gmail.com>
2025-05-05 20:31:22 +0200

    Создаёт компонент кнопки

---
+++ src/components/AppButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button class="button" type="button">
+    <slot />
+  </button>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.button {
+  min-width: 335px;
+  padding: 16px;
+  background: var(--c-primary);
+  font-size: 24px;
+  line-height: 1;
+  font-family: var(--font);
+  color: white;
+  border: none;
+  border-radius: 100px;
+  cursor: pointer;
+}
+</style>
